8bb53322febf775f3c12dc13be7038034935a818
Dylan-Kaneshiro <dylan.kaneshiro14@gmail.com>
2024-02-10 00:05:28 -0600

    video 6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // cannot output booleans or objects
-  const link = "http://www.google.com"
-  
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Site</a>
       </div>
     </div>
   );
