11e06987dade2c12c9cc92f3d0b3b4c90a2bb3ad
Dylan-Kaneshiro <dylan.kaneshiro14@gmail.com>
2024-04-16 18:12:28 -0500

    make donation form look nice

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,14 @@
 import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 import blogIDToURL from "./Services/blogIDToURL";
+import { useState } from 'react';
 
 const BlogDetails = () => {
 
     const { id } = useParams();
     const { data: blog, isPending, error } = useFetch(blogIDToURL(id))
     const history = useHistory();
+    const [donationInfo, setDonationInfo] = useState({amount:'', cardNumber: '', cardHolder: '', expiryDate: '', cvv: ''});
 
     const handleClick = () => {
         fetch(blogIDToURL(blog.id), {
@@ -14,6 +16,15 @@ const BlogDetails = () => {
         }).then(() => {
             history.push('/')
         })
+    }
+
+    const handleInputChange = (event) => {
+        setDonationInfo({...donationInfo, [event.target.name]: event.target.value});
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        // Process the donation here
     }
 
     return ( 
@@ -33,6 +44,30 @@ const BlogDetails = () => {
                     <button onClick={handleClick}>delete</button>
                 </article>
             )}
+            {blog && (
+                <div className="create">
+                    <form onSubmit={handleFormSubmit}>
+                        <h1>Donate now!</h1>
+                        <br />
+                        <label>Amount (USD):</label>
+                        <input type="number" min="0.01" step="0.01" name="amount" value={donationInfo.amount} onChange={handleInputChange} placeholder="Donation Amount" required />
+                        <label>Card Holder Name:</label>
+                        <input type="text" name="cardHolder" value={donationInfo.cardHolder} onChange={handleInputChange} placeholder="Card Holder" required />
+                        <label>Card Number:</label>
+                        <input type="number" name="cardNumber" value={donationInfo.cardNumber} onChange={handleInputChange} placeholder="Card Number" required />
+                        <label>Expiry Date:</label>
+                        <input type="month" name="expiryDate" value={donationInfo.expiryDate} onChange={handleInputChange} placeholder="Expiry Date" required />
+                        <label>CVV:</label>
+                        <input type="number" name="cvv" value={donationInfo.cvv} onChange={handleInputChange} placeholder="CVV" required />
+                        <button type="submit">Donate</button>
+                    </form>
+                </div>
+            )}
+            <p>{donationInfo['amount']}</p>
+            <p>{donationInfo['cardNumber']}</p>
+            <p>{donationInfo['cardHolder']}</p>
+            <p>{donationInfo['expiryDate']}</p>
+            <p>{donationInfo['cvv']}</p>
         </div>
      );
 }
