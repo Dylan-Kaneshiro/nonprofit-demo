3aace27624f2c27d0ed265ba7df8b01c7f4c47f3
Dylan-Kaneshiro <dylan.kaneshiro14@gmail.com>
2024-04-29 23:57:05 -0500

    Deactivate blog works

--- src/main/frontend/src/MyBlogList.jsx
+++ src/main/frontend/src/MyBlogList.jsx
@@ -1,16 +1,43 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import deactivateURL from "./Services/deactivateURL";
 
 const MyBlogList = ({blogs}) => {
     
+    const navigate = useNavigate();
+
+    const handleActiveBlog = (id) => {
+        
+    };
+
+    const handleInactiveBlog = (id) => {
+
+        fetch(deactivateURL(id), {
+            method: 'DELETE',
+            headers: { "Content-Type": "application/json" },
+        }).then(() => {
+            console.log(`blog ${id} deactivated`);
+            navigate('/');
+        })
+    };
+
     return ( 
         <div className="blog-list"> 
             {blogs.map(blog => (
                 <div className="my-blog-preview" key={blog.id}>
-                    <Link to={`/blogs/${blog.id}`}>
+
                         <h2>{blog.title}</h2>
-                        <p>{blog.author}</p>
                         <p>{blog.city}</p>
-                    </Link>
+                        {blog.active === 'A' ? (
+                            <button onClick={() => handleInactiveBlog(blog.id)}>
+                                Close Event
+                            </button>
+                        ) : (
+                            <button onClick={() => handleActiveBlog(blog.id)}>
+                                Re-Open Event
+                            </button>
+                        )}
+
                 </div>
             ))}
         </div>
